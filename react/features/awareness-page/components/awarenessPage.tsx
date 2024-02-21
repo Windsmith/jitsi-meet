f30395064d0502e196bd0a02cdd2fa6163d47a75
m4zin <mazin.sherif@gmail.com>
2024-02-21 19:14:09 +0400

    added awareness page component

---
+++ react/features/awareness-page/components/awarenessPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AwarenessPage extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>
+                    Hello world!
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default AwarenessPage
