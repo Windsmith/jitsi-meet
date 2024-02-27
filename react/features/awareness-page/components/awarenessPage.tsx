79486ac090539c3ecdce563d4675291137eaec6d
m4zin <mazin.sherif@gmail.com>
2024-02-27 16:44:38 +0400

    Audio qns & displaying scoreboard on completion for awareness game added

--- react/features/awareness-page/components/awarenessPage.tsx
+++ react/features/awareness-page/components/awarenessPage.tsx
@@ -1,24 +1,97 @@
 import React from 'react';
-import "../components/awarenessPage.css";
-import NavBar from '../components/navbar/NavBar'; 
-import Audio from '../components/audioBar/Audio';
-import Questionnaire from './questionnaire/Questionnaire';
-import Heading from '../components/heading/Heading';
-class AwarenessPage extends React.Component {
-    render() {
-        return(
-            <body>
-            <NavBar/>
-            <Heading/>
-            <Audio audio={'./components/audioBar/Recording.m4a'}/>
-            <Questionnaire isdeepfake={true}/>
-            <Audio audio={'./components/audioBar/Recording.m4a'}/>
-            <Questionnaire isdeepfake={false}/>
-            <Audio audio={'./components/audioBar/Recording.m4a'}/>
-            <Questionnaire isdeepfake={false}/>
-            </body>
-        )
-    }
-}
+import { useState } from 'react'
+import { quiz } from './questions'
+import './awarenessPage.css'
+import ReactAudioPlayer from 'react-audio-player';
 
-export default AwarenessPage
+const AwarenessPage = () => {
+    const [activeQuestion, setActiveQuestion] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+    const [showResult, setShowResult] = useState(false);
+    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+    const [result, setResult] = useState({
+        score: 0,
+        correctAnswers: 0,
+        wrongAnswers: 0,
+    });
+
+    const { questions } = quiz;
+    const { choices, correctAnswer } = questions[activeQuestion];
+    const { audioSrc } = questions[activeQuestion]; // Each question has an audioSrc property
+
+    const onClickNext = () => {
+        setSelectedAnswerIndex(null);
+        setResult((prev) =>
+            selectedAnswer
+                ? {
+                    ...prev,
+                    score: prev.score + 5,
+                    correctAnswers: prev.correctAnswers + 1,
+                }
+                : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+        );
+        if (activeQuestion !== questions.length - 1) {
+            setActiveQuestion((prev) => prev + 1);
+        } else {
+            setActiveQuestion(0);
+            setShowResult(true);
+        }
+    };
+
+    const onAnswerSelected = (answer, index) => {
+        setSelectedAnswerIndex(index);
+        if (answer === correctAnswer) {
+            setSelectedAnswer(true);
+        } else {
+            setSelectedAnswer(false);
+        }
+    };
+
+    const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
+
+    return (
+        <div className="background-stuff">
+            <div className="quiz-container">
+                {!showResult ? (
+                    <div>
+                        <div>
+                            <span className="active-question-no">{addLeadingZero(activeQuestion + 1)}</span>
+                            <span className="total-question">/{addLeadingZero(questions.length)}</span>
+                        </div>
+                        {/*Cycles through next audio.*/}
+                        <ReactAudioPlayer src={audioSrc} autoPlay controls />
+                        <ul>
+                            {choices.map((answer, index) => (
+                                <li
+                                    onClick={() => onAnswerSelected(answer, index)}
+                                    key={answer}
+                                    className={selectedAnswerIndex === index ? 'selected-answer' : null}
+                                >
+                                    {answer}
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="flex-right">
+                            <button onClick={onClickNext} disabled={selectedAnswerIndex === null}>
+                                {activeQuestion === questions.length - 1 ? 'Finish' : 'Next'}
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="result">
+                        <h3>Result</h3>
+                        <p>Total Question: <span>{questions.length}</span></p>
+                        <p>Total Score:<span> {result.score}</span></p>
+                        <p>Correct Answers:<span> {result.correctAnswers}</span></p>
+                        <p>Wrong Answers:<span> {result.wrongAnswers}</span></p>
+                        <a href={'/'}>Go back to homepage.</a>
+                        <br/>
+                        <a href={'/awareness-page'}>or try again!</a>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default AwarenessPage;
---
+++ react/features/awareness-page/components/questions.tsx
@@ -0,0 +1,40 @@
+// Question Types
+// 1. MCQs | Multiple Choice | single
+
+export const quiz = {
+    topic: 'Awareness Game.',
+    totalQuestions: 5,
+    perQuestionScore: 5,
+    questions: [
+        {
+            choices: ['Yes, it is a deepfake.', 'No, it is not a deepfake.'],
+            type: 'MCQs',
+            correctAnswer: 'Yes, it is a deepfake.',
+            audioSrc: '../Audio/sampleAudio.mp3'
+        },
+        {
+            choices: ['Yes, it is a deepfake.', 'No, it is not a deepfake.'],
+            type: 'MCQs',
+            correctAnswer: 'Yes, it is a deepfake.',
+            audioSrc: '../Audio/sampleAudio.mp3'
+        },
+        {
+            choices: ['Yes, it is a deepfake.', 'No, it is not a deepfake.'],
+            type: 'MCQs',
+            correctAnswer: 'Yes, it is a deepfake.',
+            audioSrc: '../Audio/sampleAudio.mp3'
+        },
+        {
+            choices: ['Yes, it is a deepfake.', 'No, it is not a deepfake.'],
+            type: 'MCQs',
+            correctAnswer: 'Yes, it is a deepfake.',
+            audioSrc: '../Audio/sampleAudio.mp3'
+        },
+        {
+            choices: ['Yes, it is a deepfake.', 'No, it is not a deepfake.'],
+            type: 'MCQs',
+            correctAnswer: 'Yes, it is a deepfake.',
+            audioSrc: '../Audio/sampleAudio.mp3'
+        },
+    ],
+}
